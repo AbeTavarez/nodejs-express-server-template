4468f6b903986bf676df9d0cb7d38d9e5712a928
Abraham E. Tavarez <abeincwireless@gmail.com>
2024-02-26 12:50:52 -0500

    Line 19 at index.js was updated to false instead of true, that changes the query string library, which is the recommended one from the documentation.: app.use(express.urlencoded({extended: false})
    This has no effect on how the application works now, because this is not
    being used but this change was made to to match the reccomended setup
    from the documentation. See more info here: https://www.npmjs.com/package/body-parser#examples

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ app.use(express.static('public'));
 app.use(morgan('dev'));
 app.use(helmet());
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({extended: false})); 
 
 
 // === Routes ====
